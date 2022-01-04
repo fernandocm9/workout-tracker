b3c5b86c13069218ebde994e86ea2112ca3c2ada
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-01-04 16:30:09 -0600

    added edit btn

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.delete('/workouts/:id', async(req, res)=>{
         res.json({message: `workout ${req.params.id} was deleted`})
         client.release()
     } catch (err) {
-        // fiveHundredError(err, res)
-        console.log(err.message)
+        fiveHundredError(err, res)
+       
     }
 })
 
