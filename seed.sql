f0c88e1211ce872e6add79d14a0d6d40a94bff4f
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-01-04 11:13:32 -0600

    changes to flex

--- seed.sql
+++ seed.sql
@@ -1,1 +1,2 @@
 INSERT INTO workout (the_day, type_workout, exercise, sets, reps) VALUES ('2020-01-03', 'chest', 'bench', 1, 10);
+INSERT INTO workout (the_day, type_workout, exercise, sets, reps) VALUES ('2020-01-04', 'leg', 'squat', 5, 20);
