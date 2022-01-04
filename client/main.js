0a0bc97f653fd013be237f46f39de8bee31ad6b6
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-01-03 20:10:43 -0600

    try jquery

--- client/main.js
+++ client/main.js
@@ -2,4 +2,7 @@ const weightBtn = document.querySelector('#weight')
 const cardioBtn = document.querySelector('#cardio')
 const deleteBtn = document.querySelector('#delete')
 
-// ('div').appendTo('body')
+// const submitCont = document.createElement('div')
+// (submitCont).attr
+
+$('div').appendTo('body')
